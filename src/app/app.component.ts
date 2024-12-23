ebdac1417e5f71a000fc3900871847f2eb3417f7
njorquera98 <njorquera98.dev@gmail.com>
2024-12-23 04:21:23 -0300

    feat(navbar) crear componente navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Trisquelion';
+  constructor(private router: Router) { } // Inyecta el Router en el constructor
+
+  // Método para verificar si la ruta actual es '/login'
+  isLoginPage(): boolean {
+    return this.router.url === '/login'; // Ahora puedes usar this.router
+  }
 }
---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css'
+})
+export class NavbarComponent {
+  constructor(private authService: AuthService, private router: Router) { }
+
+  logout(): void {
+    this.authService.logout();  // Elimina el token del almacenamiento
+    this.router.navigate(['/login']);  // Redirige a la ruta de login
+  }
+}
