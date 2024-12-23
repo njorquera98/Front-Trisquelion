fd3410409695cca50ea5a592ddb60a81f46e6760
njorquera98 <njorquera98.dev@gmail.com>
2024-12-23 03:09:04 -0300

    feat(frontend): crear componente de login
    Se creó el componente de login en el frontend, con los campos básicos de
    autenticación (usuario y contraseña) y las primeras interacciones con el
    servicio de autenticación.

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  imports: [FormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+  email: string = '';
+  password: string = '';
+
+  constructor(private authService: AuthService, private router: Router) { }
+
+  onSubmit() {
+    this.authService.login(this.email, this.password).subscribe({
+      next: (response) => {
+        console.log('Login exitoso:', response);
+
+        // Guarda el token en localStorage o sessionStorage
+        this.authService.saveToken(response.accessToken);
+
+        // Redirige a la página de pacientes activos
+        this.router.navigate(['/activos']);
+      },
+      error: (err) => {
+        console.error('Error en el login:', err);
+        alert('Credenciales incorrectas o error en el servidor');
+      },
+    });
+  }
+}
