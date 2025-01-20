ac59eed1acfbf6a46ddfd3ae8b3480c5af8500f1
njorquera98 <njorquera98.dev@gmail.com>
2025-01-20 02:36:41 -0300

    feat(asistencia) agregar toggle estado paciente

--- src/app/asistencia/asistencia.component.ts
+++ src/app/asistencia/asistencia.component.ts
@@ -1,15 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PacienteService } from '../services/paciente.service';
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { PacienteComponent } from '../paciente/paciente.component';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-asistencia',
-  imports: [FormsModule, CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './asistencia.component.html',
   styleUrl: './asistencia.component.css'
 })
-export class AsistenciaComponent {
+export class AsistenciaComponent implements OnInit {
   toggleActivo: boolean = true;
+  pacienteId!: number;
 
   // Configuración inicial
   dias: string[] = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'];
@@ -18,14 +22,41 @@ export class AsistenciaComponent {
   // Estado de asistencia: Almacena días y horarios seleccionados
   asistencia: { [dia: string]: string[] } = {};
 
-  constructor() {
+  constructor(
+    private pacienteService: PacienteService,
+    private route: ActivatedRoute,
+    private pacienteComponent: PacienteComponent // Inyectamos el componente PacienteComponent
+  ) { }
+
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.pacienteId = +params['id']; // Obtener el pacienteId de la URL
+      console.log('Paciente ID:', this.pacienteId);
+    });
+
     // Inicializar el estado de asistencia con arrays vacíos
     this.dias.forEach(dia => {
       this.asistencia[dia] = [];
     });
   }
+
   onToggleChange(): void {
     console.log('Toggle cambiado:', this.toggleActivo);
+    this.actualizarEstadoPaciente(this.toggleActivo);
+  }
+
+  // Llamada al servicio para actualizar el estado del paciente
+  actualizarEstadoPaciente(estado: boolean): void {
+    this.pacienteService.updatePacienteEstado(this.pacienteId, estado).subscribe(
+      (response) => {
+        console.log('Estado del paciente actualizado:', response);
+        // Después de actualizar el estado, recargamos la información del paciente
+        this.pacienteComponent.cargarPaciente(this.pacienteId); // Llamamos al método cargarPaciente del componente PacienteComponent
+      },
+      (error) => {
+        console.error('Error al actualizar el estado del paciente:', error);
+      }
+    );
   }
 
   // Manejar selección múltiple de horarios
